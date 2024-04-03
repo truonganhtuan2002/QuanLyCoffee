230255d03bea56753b1a89ab1de6817095ec3183
Trương Anh Tuấn <121568773+truonganhtuan2002@users.noreply.github.com>
2024-04-03 19:08:56 +0700

    Add files via upload

---
+++ BUS_DrinkType.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Windows.Forms;
+using Model;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    public sealed class BUS_DrinkType : BUS<BUS_DrinkType, DrinkType, int>
+    {
+        public override DataTable Read()
+        {
+            return DAL_DrinkType.Instance.Read();
+        }
+        public override void GetList(ManageList<DrinkType> list)
+        {
+            list.Clear();
+            foreach (DataRow row in Read().Rows)
+            {
+                list.Add(new DrinkType(row));
+            }
+        }
+        public override void Create(DrinkType newDrinkType)
+        {
+            DAL_DrinkType.Instance.Create(newDrinkType);
+        }
+        public override void Delete(int deleteDrinkTypeID)
+        {
+            DAL_DrinkType.Instance.Delete(deleteDrinkTypeID);
+        }
+        public override void Update(DrinkType updateDrinkType, int oldDrinkTypeID)
+        {
+            DAL_DrinkType.Instance.Update(updateDrinkType, oldDrinkTypeID);
+        }
+        //Load danh sach DrinkType vao ComboBox
+        public void LoadDrinkTypeToComboBox(ManageList<DrinkType> list,ComboBox cb)
+        {
+            cb.Items.Clear();
+            for (int i = 0; i < list.Count; i++)
+            {
+                cb.Items.Add(list[i].Name);
+            }
+        }        
+    }
+}
